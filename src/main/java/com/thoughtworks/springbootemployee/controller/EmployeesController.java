956e3e55616a68215daf8ca906dbabe3ffd5b905
magluar <aryann.magluyan@oocl.com>
2021-08-11 16:30:48 +0800

    [magluar] add get employees by pagination method

--- src/main/java/com/thoughtworks/springbootemployee/controller/EmployeesController.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/EmployeesController.java
@@ -1,13 +1,11 @@
 package com.thoughtworks.springbootemployee.controller;
 
 import com.thoughtworks.springbootemployee.controller.Employee;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/employees")
@@ -17,6 +15,10 @@ public class EmployeesController {
     public EmployeesController(){
         employees.add(new Employee(1, "alice", 20, "female", 2000));
         employees.add(new Employee(2, "bob", 21, "male", 1000));
+        employees.add(new Employee(3, "tom", 5, "male", 5));
+        employees.add(new Employee(4, "jerry", 4, "male", 5));
+        employees.add(new Employee(5, "doraemon", 99, "male", 10));
+        employees.add(new Employee(6, "nobita", 10, "male", 100));
     }
 
     @GetMapping
@@ -30,5 +32,13 @@ public class EmployeesController {
                 .filter(employee -> employee.getId().equals(employeeId))
                 .findFirst()
                 .orElse(null);
+    }
+
+    @GetMapping(params = {"pageIndex", "pageSize"}) //1 5
+    public List<Employee> getEmployeesByPagination(@RequestParam Integer pageIndex, @RequestParam Integer pageSize){
+        return employees.stream()
+                .skip((pageIndex - 1) * pageSize)
+                .limit(pageSize)
+                .collect(Collectors.toList());
     }
 }
