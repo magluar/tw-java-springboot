758112c79e220084269624741d0715e03f683764
magluar <aryann.magluyan@oocl.com>
2021-08-14 13:42:43 +0800

    [company][magluar] feat: should_return_updated_company_given_updateCompanyRecord_given_update_request_body

--- src/test/java/com/thoughtworks/springbootemployee/service/CompanyServiceTest.java
+++ src/test/java/com/thoughtworks/springbootemployee/service/CompanyServiceTest.java
@@ -24,7 +24,7 @@ public class CompanyServiceTest {
     private CompanyRepository companyRepository;
 
     @Test
-    public void should_return_all_companies_when_getAllCompanies_api(){
+    public void should_return_all_companies_when_getAllCompanies_given_company(){
         //given
         List<Employee> employees = new ArrayList<>();
         List<Employee> emptyEmployees = new ArrayList<>();
@@ -47,7 +47,7 @@ public class CompanyServiceTest {
     }
 
     @Test
-    public void should_return_companies_by_id_when_findCompanyById_api(){
+    public void should_return_companies_by_id_when_findCompanyById_given_company_id(){
         //given
         List<Employee> employees = new ArrayList<>();
         List<Employee> emptyEmployees = new ArrayList<>();
@@ -70,7 +70,7 @@ public class CompanyServiceTest {
     }
 
     @Test
-    public void should_return_companies_by_page_and_size_when_getCompanies_by_pagination_api(){
+    public void should_return_companies_by_page_and_size_when_getCompanies_by_pagination_given_page_and_page_size(){
         //given
         List<Employee> employees = new ArrayList<>();
         List<Employee> emptyEmployees = new ArrayList<>();
@@ -96,7 +96,7 @@ public class CompanyServiceTest {
     }
 
     @Test
-    public void should_return_employees_when_findEmployeesByCompanyId(){
+    public void should_return_employees_when_findEmployeesByCompanyId_given_company_id(){
         //given
         List<Employee> employees = new ArrayList<>();
         List<Employee> emptyEmployees = new ArrayList<>();
@@ -121,7 +121,7 @@ public class CompanyServiceTest {
     }
 
     @Test
-    public void should_return_added_company_when_addCompany_api(){
+    public void should_return_added_company_when_addCompany_given_added_company(){
         //given
         List<Employee> employees = new ArrayList<>();
         List<Employee> emptyEmployees = new ArrayList<>();
@@ -143,5 +143,29 @@ public class CompanyServiceTest {
 
         //then
         assertEquals(3, companySize.size());
+    }
+
+    @Test
+    public void should_return_updated_company_given_updateCompanyRecord_given_update_request_body(){
+        //given
+        List<Employee> employees = new ArrayList<>();
+        List<Employee> emptyEmployees = new ArrayList<>();
+        List<Company> companies = new ArrayList<>();
+        Employee employee1 = new Employee(1, "alice", 20, "female", 2000, 1);
+        Employee employee2 = new Employee(2, "bob", 21, "male", 1000, 1);
+        employees.add(employee1);
+        employees.add(employee2);
+        Company google = new Company(companies.size() + 1, "Google", employees);
+        companies.add(google);
+        Company amazon = new Company(companies.size() + 1, "Amazon", emptyEmployees);
+        companies.add(amazon);
+        given(companyRepository.getCompanies()).willReturn(companies);
+
+        //when
+        Company updatedCompany = new Company(null, "Netflix", null);
+        Company updatedCompany1 = companyService.updateCompany(1, updatedCompany);
+
+        //then
+        assertEquals(companyService.findCompanyById(1).getCompanyName(), updatedCompany1.getCompanyName());
     }
 }
